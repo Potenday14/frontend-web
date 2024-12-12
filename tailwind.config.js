7cd3e379ce6b8a2ecee9edfa05ebb7f064b51c02
qaws7791 <qaws7791@gmail.com>
2024-12-13 04:44:23 +0900

    feat: tailwind.config.js에 색상 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       spacing: {
         container: "37.5rem",
+      },
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: {
+        50: "#f6f6f6",
+        100: "#e7e7e7",
+        200: "#d1d1d1",
+        300: "#b0b0b0",
+        400: "#888888",
+        500: "#6d6d6d",
+        600: "#5d5d5d",
+        700: "#4f4f4f",
+        800: "#454545",
+        900: "#3d3d3d",
+        950: "#0e0e0e",
       },
     },
   },
