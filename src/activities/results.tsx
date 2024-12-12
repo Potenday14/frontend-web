86207de44d00aeb5070e45347eec9d24904667bc
qaws7791 <qaws7791@gmail.com>
2024-12-13 06:36:22 +0900

    feat: FixedBottom 컴포넌트에 스크롤 이벤트 추가하여 go to top 구현

--- src/activities/results.tsx
+++ src/activities/results.tsx
@@ -6,6 +6,10 @@ import { useFlow } from "../stackflow";
 import TimeCircle from "../assets/time-circle.svg?react";
 import Calorie from "../assets/calorie.svg?react";
 import { useFetchRecommendation } from "../hooks/queries";
+import ArrowUp from "../assets/arrow-up.svg?react";
+import { useRef, useState } from "react";
+import FixedBottom from "../components/layout/fixed-bottom";
+import { cn } from "../components/utils";
 type ResultsActivityParams = {
   id: number;
 };
@@ -13,79 +17,105 @@ type ResultsActivityParams = {
 const ResultsActivity: ActivityComponentType<ResultsActivityParams> = ({
   params,
 }) => {
+  const [scrolled, setScrolled] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
   const queryClient = useQueryClient();
   const flow = useFlow();
   const { data: recommendation } = useFetchRecommendation({
     recommendationId: params.id,
   });
+
   return (
     <AppScreen appBar={{ title: "레시피 추천 결과" }}>
-      <div className="p-4">
-        <h1 className="sr-only">추천 결과</h1>
+      <FixedBottom
+        onScroll={(e) => {
+          console.log("Page scroll: ", e.currentTarget.scrollTop);
+          const scrolled = e.currentTarget.scrollTop > 0;
+          console.log("Scrolled: ", scrolled);
+          setScrolled(scrolled);
+        }}
+      >
+        <div className="relative p-4 h-full group" ref={containerRef}>
+          <h1 className="sr-only">추천 결과</h1>
 
-        <p className="text-lg bg-gray-50 py-6 px-4 rounded-xl result__reason">
-          {recommendation.reason}
-        </p>
+          <p className="text-lg bg-gray-50 py-6 px-4 rounded-xl result__reason">
+            {recommendation.reason}
+          </p>
 
-        <div className="mt-[18px]">
-          {recommendation.recipes.map((recipe) => (
-            <div
-              key={recipe.id}
-              className="grid grid-cols-12 gap-4 p-5 border border-gray-100 rounded-xl mt-4"
-            >
-              <div className="flex flex-col justify-between col-span-7">
-                <div>
-                  <h3 className="text-xl font-semibold line-clamp-2">
-                    {recipe.name}
-                  </h3>
-                  <div className="mt-3 flex flex-col gap-1.5">
-                    <dl className="flex gap-3 items-center">
-                      <dt>
-                        <TimeCircle />
-                        <span className="sr-only">조리 시간</span>
-                      </dt>
-                      <dd className="text-sm">{recipe.time}분</dd>
-                    </dl>
-                    <dl className="flex gap-3 items-center">
-                      <dt>
-                        <Calorie />
-                        <span className="sr-only">칼로리</span>
-                      </dt>
-                      <dd className="text-sm">{recipe.calories}kcal</dd>
-                    </dl>
+          <div className="mt-[18px]">
+            {recommendation.recipes.map((recipe) => (
+              <div
+                key={recipe.id}
+                className="grid grid-cols-12 gap-4 p-5 border border-gray-100 rounded-xl mt-4"
+              >
+                <div className="flex flex-col justify-between col-span-7">
+                  <div>
+                    <h3 className="text-xl font-semibold line-clamp-2">
+                      {recipe.name}
+                    </h3>
+                    <div className="mt-3 flex flex-col gap-1.5">
+                      <dl className="flex gap-3 items-center">
+                        <dt>
+                          <TimeCircle />
+                          <span className="sr-only">조리 시간</span>
+                        </dt>
+                        <dd className="text-sm">{recipe.time}분</dd>
+                      </dl>
+                      <dl className="flex gap-3 items-center">
+                        <dt>
+                          <Calorie />
+                          <span className="sr-only">칼로리</span>
+                        </dt>
+                        <dd className="text-sm">{recipe.calories}kcal</dd>
+                      </dl>
+                    </div>
                   </div>
+                  <button
+                    className="rounded-full bg-gray-950 p-2.5 whitespace-nowrap text-xs text-white"
+                    onClick={async () => {
+                      if (
+                        !queryClient.getQueryData([
+                          "recipe",
+                          { recipeId: recipe.id },
+                        ])
+                      ) {
+                        await queryClient.prefetchQuery({
+                          queryKey: ["recipe", { recipeId: recipe.id }],
+                          queryFn: async () => {
+                            return fetchRecipe(recipe.id);
+                          },
+                        });
+                      }
+                      flow.push("RecipeActivity", { id: recipe.id });
+                    }}
+                  >
+                    전체 레시피 확인하기
+                  </button>
                 </div>
-                <button
-                  className="rounded-full bg-gray-950 p-2.5 whitespace-nowrap text-xs text-white"
-                  onClick={async () => {
-                    if (
-                      !queryClient.getQueryData([
-                        "recipe",
-                        { recipeId: recipe.id },
-                      ])
-                    ) {
-                      await queryClient.prefetchQuery({
-                        queryKey: ["recipe", { recipeId: recipe.id }],
-                        queryFn: async () => {
-                          return fetchRecipe(recipe.id);
-                        },
-                      });
-                    }
-                    flow.push("RecipeActivity", { id: recipe.id });
-                  }}
-                >
-                  전체 레시피 확인하기
-                </button>
+                <img
+                  src={recipe.mainPhoto}
+                  alt={recipe.name}
+                  className="rounded-[20px] col-span-5 min-h-44 w-full object-cover"
+                />
               </div>
-              <img
-                src={recipe.mainPhoto}
-                alt={recipe.name}
-                className="rounded-[20px] col-span-5 min-h-44 w-full object-cover"
-              />
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+        <button
+          className={cn(
+            "fixed bottom-6 right-4 p-2 bg-gray-950 text-white rounded-full",
+            scrolled ? "block" : "hidden"
+          )}
+          onClick={() => {
+            containerRef.current?.parentElement?.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
+        >
+          <ArrowUp />
+        </button>
+      </FixedBottom>
     </AppScreen>
   );
 };
--- src/components/layout/fixed-bottom.tsx
+++ src/components/layout/fixed-bottom.tsx
@@ -4,6 +4,7 @@ import { cn } from "../utils";
 export interface FixedBottomProps extends React.ComponentProps<"div"> {
   bottom?: React.ReactNode;
   bottomClassName?: string;
+  onScroll?: (e: React.UIEvent<HTMLDivElement>) => void;
 }
 
 export default function FixedBottom({
@@ -11,10 +12,12 @@ export default function FixedBottom({
   children,
   bottom,
   bottomClassName,
+  onScroll,
   ...props
 }: FixedBottomProps) {
   const realRef = useRef<HTMLDivElement>(null);
   const dummyRef = useRef<HTMLDivElement>(null);
+  const viewRef = useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     const real = realRef.current;
@@ -26,19 +29,26 @@ export default function FixedBottom({
 
   return (
     <div className="h-full flex flex-col" {...props}>
-      <div className={cn("flex-1 overflow-y-auto", className)} {...props}>
+      <div
+        ref={viewRef}
+        onScroll={onScroll}
+        className={cn("flex-1 overflow-y-auto", className)}
+        {...props}
+      >
         {children}
       </div>
       <div ref={dummyRef} />
-      <div
-        ref={realRef}
-        className={cn(
-          "fixed bottom-0 py-2 px-4 w-full flex flex-col",
-          bottomClassName
-        )}
-      >
-        {bottom}
-      </div>
+      {bottom && (
+        <div
+          ref={realRef}
+          className={cn(
+            "fixed bottom-0 pt-2 pb-[29px] px-4 w-full flex flex-col",
+            bottomClassName
+          )}
+        >
+          {bottom}
+        </div>
+      )}
     </div>
   );
 }
