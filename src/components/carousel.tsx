4ea0df124faf931f8763cde71e3cfffbc675b1f5
qaws7791 <qaws7791@gmail.com>
2024-12-13 00:55:19 +0900

    feat: 애니메이션 추가

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -1,35 +1,98 @@
 import useEmblaCarousel from "embla-carousel-react";
 import { Character } from "../mock/data";
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { DotButton, useDotButton } from "./dot-button";
 import { cn } from "./utils";
+import { EmblaCarouselType, EmblaEventType } from "embla-carousel";
 
 interface EmblaCarouselProps {
   characters: Character[];
   onSelect: (index: number) => void;
 }
 
-export function EmblaCarousel({ characters, onSelect }: EmblaCarouselProps) {
+const TWEEN_FACTOR_BASE = 0.2;
+
+const numberWithinRange = (number: number, min: number, max: number): number =>
+  Math.min(Math.max(number, min), max);
+
+export function EmblaCarousel({ characters }: EmblaCarouselProps) {
   const [emblaRef, emblaApi] = useEmblaCarousel({
     loop: true,
   });
   const { selectedIndex, scrollSnaps, onDotButtonClick } =
     useDotButton(emblaApi);
+  const tweenFactor = useRef(0);
+  const tweenNodes = useRef<HTMLElement[]>([]);
+
+  const setTweenNodes = useCallback((emblaApi: EmblaCarouselType): void => {
+    tweenNodes.current = emblaApi.slideNodes().map((slideNode) => {
+      return slideNode.querySelector(".embla__slide__img") as HTMLElement;
+    });
+    console.log("tweenNodes", tweenNodes.current);
+  }, []);
+
+  const setTweenFactor = useCallback((emblaApi: EmblaCarouselType) => {
+    tweenFactor.current = TWEEN_FACTOR_BASE * emblaApi.scrollSnapList().length;
+  }, []);
+
+  const tweenScale = useCallback(
+    (emblaApi: EmblaCarouselType, eventName?: EmblaEventType) => {
+      const engine = emblaApi.internalEngine();
+      const scrollProgress = emblaApi.scrollProgress();
+      const slidesInView = emblaApi.slidesInView();
+      const isScrollEvent = eventName === "scroll";
+
+      emblaApi.scrollSnapList().forEach((scrollSnap, snapIndex) => {
+        let diffToTarget = scrollSnap - scrollProgress;
+        const slidesInSnap = engine.slideRegistry[snapIndex];
+
+        slidesInSnap.forEach((slideIndex) => {
+          if (isScrollEvent && !slidesInView.includes(slideIndex)) return;
+
+          if (engine.options.loop) {
+            engine.slideLooper.loopPoints.forEach((loopItem) => {
+              const target = loopItem.target();
+
+              if (slideIndex === loopItem.index && target !== 0) {
+                const sign = Math.sign(target);
+
+                if (sign === -1) {
+                  diffToTarget = scrollSnap - (1 + scrollProgress);
+                }
+                if (sign === 1) {
+                  diffToTarget = scrollSnap + (1 - scrollProgress);
+                }
+              }
+            });
+          }
+
+          const tweenValue = 1 - Math.abs(diffToTarget * tweenFactor.current);
+          const scale = numberWithinRange(tweenValue, 0, 1).toString();
+          const tweenNode = tweenNodes.current[slideIndex];
+          console.log("tweenNode", tweenNode);
+          tweenNode.style.transform = `scale(${scale})`;
+        });
+      });
+    },
+    []
+  );
 
   useEffect(() => {
     if (!emblaApi) return;
 
+    setTweenNodes(emblaApi);
+    setTweenFactor(emblaApi);
+    tweenScale(emblaApi);
+
     emblaApi
-      .on("init", () => {
-        console.log("Embla Carousel initialized");
-        onSelect(0);
-      })
-      .on("select", () => {
-        const currentSlide = emblaApi.selectedScrollSnap();
-        onSelect(currentSlide);
-        console.log("Selected index:", characters[currentSlide].mood);
-      });
-  }, [emblaApi, onSelect, characters]);
+      .on("reInit", setTweenNodes)
+      .on("reInit", setTweenFactor)
+      .on("reInit", tweenScale)
+      .on("scroll", tweenScale)
+      .on("slideFocus", tweenScale);
+  }, [emblaApi, tweenScale]);
+
+  const currentCharacter = characters[selectedIndex];
 
   return (
     <div className="embla">
@@ -38,7 +101,7 @@ export function EmblaCarousel({ characters, onSelect }: EmblaCarouselProps) {
           {characters.map((character) => (
             <div
               key={character.id}
-              className="embla__slide flex-shrink-0 flex-grow-0 basis-full flex items-center justify-center"
+              className="embla__slide flex-shrink-0 flex-grow-0 basis-6/12 flex items-center justify-center"
             >
               <img
                 src={character.image}
@@ -49,7 +112,10 @@ export function EmblaCarousel({ characters, onSelect }: EmblaCarouselProps) {
           ))}
         </div>
       </div>
-      <div className="embla__dots flex flex-wrap justify-center items-center gap-[9px]">
+      <div className="flex items-center text-center justify-center mt-4">
+        {currentCharacter.mood}
+      </div>
+      <div className="embla__dots flex flex-wrap justify-center items-center gap-[9px] mt-4">
         {scrollSnaps.map((_, index) => (
           <DotButton
             key={index}
