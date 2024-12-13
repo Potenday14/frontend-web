cb7d67b59dcfb2c60cea1874a2b83c156bcb7566
qaws7791 <qaws7791@gmail.com>
2024-12-14 03:15:41 +0900

    feat: 환경 변수 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="./vite-env-override.d.ts" />
 /// <reference types="vite-plugin-svgr/client" />
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_BASE_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
