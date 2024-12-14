99837faeb1847186741f0947e5cefc5c6776a0b5
qaws7791 <qaws7791@gmail.com>
2024-12-14 21:42:05 +0900

    feat: 로딩 페이지 디자인 변경

--- src/activities/result-loading.tsx
+++ src/activities/result-loading.tsx
@@ -1,11 +1,11 @@
 import { ActivityComponentType } from "@stackflow/react";
-import { AppScreen } from "@stackflow/plugin-basic-ui";
 import LoadingSpinner from "../components/loading-spinner";
 import { Helmet } from "react-helmet";
+import Screen from "../components/screen";
 
 const ResultLoadingActivity: ActivityComponentType = () => {
   return (
-    <AppScreen>
+    <Screen className="bg-gray-950">
       <Helmet>
         <title>결과를 불러오는 중...</title>
       </Helmet>
@@ -16,12 +16,12 @@ const ResultLoadingActivity: ActivityComponentType = () => {
 
         <LoadingSpinner />
 
-        <p className="text-xs text-center">
+        <p className="text-xs text-center text-white">
           선택하신 2개의 재료가 모두 들어간 레시피가 없는 경우 1개의 재료가
           들어간 레시피가 나올 수 있습니다.
         </p>
       </div>
-    </AppScreen>
+    </Screen>
   );
 };
 
